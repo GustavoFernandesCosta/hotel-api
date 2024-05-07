64f459948f97aed9e198349643849f71cce65e57
root <root@GustavoCosta>
2024-05-07 13:35:37 -0300

    feat: add express innitial server

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello, world!");
+import express from "express";
+import { config } from "dotenv";
+
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => console.log(`Server is running on port ${port}`));
